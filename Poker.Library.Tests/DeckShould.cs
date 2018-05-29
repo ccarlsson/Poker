052afc46fefe76345b0324be25496196b1e34bcb
Christer Carlsson <ccarlsson@outlook.com>
2018-05-29 20:55:04 +0200

    Add test

--- Poker.Library.Tests/DeckShould.cs
+++ Poker.Library.Tests/DeckShould.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Poker.Library;
 using Xunit;
 
@@ -17,7 +18,11 @@ namespace Poker.Library.Tests
             return 0;
         }
     }
-    public class DeckShould {
+
+
+
+    public class DeckShould
+    {
 
         [Fact]
         public void Deal2OfHearts()
@@ -30,6 +35,29 @@ namespace Poker.Library.Tests
             var card = deck.Deal;
         //Then
             Assert.Equal(expected, card.ShortName);
+        }
+
+
+        [Fact]
+        public void Have47CardsAfterDealingAHand()
+        {
+            var deck = new Deck(new RandomizerStub());
+            var expected = 47;
+
+            var p = Enumerable.Range(0, 5).Select(x => deck.Deal).ToList();
+
+            var actual = deck.CardsLeft;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Have52Cards()
+        {
+            var deck = new Deck(new RandomizerStub());
+            var expected = 52;
+
+            var actual = deck.CardsLeft;
+            Assert.Equal(expected, actual);
         }
     }
 }
